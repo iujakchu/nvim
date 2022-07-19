0e56e945f1de94f26fff89753c4bc0d4fbd2234f
iujakchu <iujakchu@163.com>
2022-07-19 20:57:28 +0800

    chore:set default font on windows

--- lua/jak/neovide.lua
+++ lua/jak/neovide.lua
@@ -6,3 +6,4 @@ vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
 vim.g.neovide_fullscreen = true
 vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.o.guifont = "Iosevka:h15"
