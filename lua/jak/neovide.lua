aefa7a1adc57ffeea621f4f41c78977ce60e13c5
iujakchu <iujakchu@163.com>
2022-07-21 16:53:29 +0800

    fix: neovim on windows dees not support nushell

--- lua/jak/neovide.lua
+++ lua/jak/neovide.lua
@@ -1,6 +1,8 @@
 if vim.g.neovide == nil then
     return
 end
+-- nushell does not support on windows
+vim.g.shell = "cmd"
 vim.g.neovide_transparency = 0.8
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
