6e7826037d602cdad8145ba1ff561bde0ce00f84
iujakchu <iujakchu@163.com>
2022-07-27 21:25:59 +0800

    fix: which-key to map general mappings

--- lua/jak/mappings.lua
+++ lua/jak/mappings.lua
@@ -3,30 +3,30 @@ local Map = {}
 vim.g["mapleader"] = " "
 Map.general = {
     n = {
-        ["<ESC>"] = ":noh<CR>",
-        ["Q"] = ":q<CR>",
-        ["S"] = ":w<CR>",
-        ["<leader>."] = ":so %<CR>",
-        ["n"] = "nzz",
-        ["N"] = "Nzz",
-        ["<leader>d"] = ":bdelete<CR>",
-        ["<C-t>"] = ":tabe<CR>",
-        ["<leader>-"] = ":-tabnext<CR>",
-        ["<leader>="] = ":+tabnext<CR>",
-        ["<C-j>"] = "<C-w>j",
-        ["<C-k>"] = "<C-w>k",
-        ["<C-h>"] = "<C-w>h",
-        ["<C-l>"] = "<C-w>l",
+        ["<ESC>"] = { ":noh<CR>", "remove hl" },
+        ["Q"] = { ":q<CR>", "quit" },
+        ["S"] = { ":w<CR>", "sive file" },
+        ["<leader>."] = { ":so %<CR>", "source current file" },
+        ["n"] = { "nzz", "next item with focus" },
+        ["N"] = { "Nzz", "previous item with focus" },
+        ["<leader>d"] = { ":bdelete<CR>", "delete current buffer" },
+        ["<C-t>"] = { ":tabe<CR>", "open a new tab" },
+        ["<leader>-"] = { ":-tabnext<CR>", "jump to previous tab" },
+        ["<leader>="] = { ":+tabnext<CR>", "jump to next tab" },
+        ["<C-j>"] = { "<C-w>j", "jump to the window below" },
+        ["<C-k>"] = { "<C-w>k", "jump to the window up" },
+        ["<C-h>"] = { "<C-w>h", "jump to the left window" },
+        ["<C-l>"] = { "<C-w>l", "jump to the right window" },
     },
     v = {
-        ["J"] = "5j",
-        ["K"] = "5k",
-        ["<"] = "<gv",
-        [">"] = ">gv",
+        ["J"] = { "5j", "fast move j in visual mode" },
+        ["K"] = { "5k", "fast move k in visual mode" },
+        ["<"] = { "<gv", "un indent lines" },
+        [">"] = { ">gv", "indent lindes" },
     },
     i = {
-        ["<A-a>"] = "<Esc>^i",
-        ["<A-e>"] = "<End>",
+        ["<A-a>"] = { "<Esc>^i", "move to the first char" },
+        ["<A-e>"] = { "<End>", "move the the end char" },
     },
 }
 
@@ -42,19 +42,19 @@ Map.general = {
 -- TODO:lspconfig
 Map.lspconfig = {
     n = {
-        ["gD"] = "<cmd>lua vim.lsp.buf.declaration()<CR>",
-        ["gd"] = "<cmd>lua vim.lsp.buf.definition()<CR>",
-        ["r"] = "<cmd>lua vim.lsp.buf.hover()<CR>",
-        ["gi"] = "<cmd>lua vim.lsp.buf.implementation()<CR>",
-        ["<C-r>"] = "<cmd>lua vim.lsp.buf.signature_help()<CR>",
-        ["<leader>rn"] = "<cmd>lua vim.lsp.buf.rename()<CR>",
-        ["gr"] = "<cmd>lua vim.lsp.buf.references()<CR>",
-        ["<rightmouse>"] = "<cmd>lua vim.lsp.buf.definition()<CR>",
-        ["<leader>ca"] = "<cmd>lua vim.lsp.buf.code_action()<CR>",
-        ["[d"] = '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>',
-        ["gl"] = "<cmd>lua vim.diagnostic.open_float()<CR>",
-        ["]d"] = '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>',
-        ["<leader>q"] = "<cmd>lua vim.diagnostic.setloclist()<CR>",
+        ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "go to decl" },
+        ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<CR>", "go to def" },
+        ["r"] = { "<cmd>lua vim.lsp.buf.hover()<CR>", "buf hover" },
+        ["gi"] = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "go to impl" },
+        ["<C-r>"] = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "signature_help" },
+        ["<leader>rn"] = { "<cmd>lua vim.lsp.buf.rename()<CR>", "rename variable" },
+        ["gr"] = { "<cmd>lua vim.lsp.buf.references()<CR>", "go to ref" },
+        ["<rightmouse>"] = { "<cmd>lua vim.lsp.buf.definition()<CR>", "rightmouse click " },
+        ["<leader>ca"] = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "code action" },
+        ["[d"] = { '<cmd>lua vim.diagnostic.goto_prev({ border =  "rounded"  })<CR>', "goto previous diag" },
+        ["]d"] = { '<cmd>lua vim.diagnostic.goto_next({ border =  "rounded"  })<CR>', "goto next diag" },
+        ["gl"] = { "<cmd>lua vim.diagnostic.open_float()<CR>", "open float diag" },
+        ["<leader>q"] = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "loc diag" },
     },
 }
 cmd "silent! command LuaSnipEdit lua require('luasnip.loaders.from_lua').edit_snippet_files()"
@@ -72,9 +72,8 @@ local default_options = {
 for _, v in pairs(Map) do
     for ik, iv in pairs(v) do
         local options = vim.tbl_deep_extend("force", default_options, { mode = ik })
-        -- wk.register(iv, options)
         for iik, iiv in pairs(iv) do
-            vim.keymap.set(ik, iik, iiv)
+            wk.register({ [iik] = iiv }, options)
         end
     end
 end
